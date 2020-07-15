4eaf286c17b225808d912ec60dfe7081ed2eda11
Luke Plant <L.Plant.98@cantab.net>
2020-07-15 10:42:42 +0300

    Code

--- code/the_right_way/urls.py
+++ code/the_right_way/urls.py
@@ -1,5 +1,5 @@
 # For more information please see:
-#     https://docs.djangoproject.com/en/3.0/topics/http/urls/
+#     https://docs.djangoproject.com/en/stable/topics/http/urls/
 from django.contrib import admin
 from django.urls import include, path
 
